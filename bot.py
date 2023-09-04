4a24cffccc20583913040b1107f6bff0da2d6ef9
Dima <dmitrij_bo05@mail.ru>
2023-09-04 22:39:59 +0500

    start

---
+++ bot.py
@@ -0,0 +1,28 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from aiologger.loggers.json import JsonLogger
+
+logger = JsonLogger.with_default_handlers(
+    level=logging.DEBUG,
+)
+
+# logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token="")
+
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
