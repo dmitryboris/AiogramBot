578bb4fe0f166929d58ba85ef567474635adaff9
Dima <dmitrij_bo05@mail.ru>
2023-09-26 21:52:41 +0500

    create aiosqlite db

--- data/create_db.py
+++ data/create_db.py
@@ -2,18 +2,35 @@ import aiosqlite
 import asyncio
 
 
-async def create(db_file):
+async def create_user_table(db_file):
     conn_str = f'sqlite:///{db_file}'
-    conn = await aiosqlite.connect(conn_str)
-    await conn.execute(
-        """CREATE TABLE users (
-        id int PRIMARY KEY  
-        """
-    )
+    async with aiosqlite.connect(db_file) as db:
+        await db.execute('CREATE TABLE IF NOT EXISTS users'
+                         '(id INTEGER PRIMARY KEY,'
+                         ' name VARCHAR,'
+                         ' count_of_orders INTEGER)')
+        await db.commit()
 
 
-async def main():
-    await create(1)
+async def create_order_table(db_file):
+    async with aiosqlite.connect(db_file) as db:
+        await db.execute('CREATE TABLE IF NOT EXISTS orders'
+                         '(id INTEGER PRIMARY KEY)')
+        await db.commit()
 
 
-asyncio.get_event_loop().run_until_complete(create('./db/test.sqlite'))
+async def create_subscription_table(db_file):
+    async with aiosqlite.connect(db_file) as db:
+        await db.execute('CREATE TABLE IF NOT EXISTS subscriptions'
+                         '(id INTEGER PRIMARY KEY,'
+                         ' name TEXT)')
+        await db.commit()
+
+
+async def main(db_file):
+    await create_user_table(db_file)
+    await create_order_table(db_file)
+    await create_subscription_table(db_file)
+
+
+asyncio.get_event_loop().run_until_complete(main('../db/bot.sqlite'))
